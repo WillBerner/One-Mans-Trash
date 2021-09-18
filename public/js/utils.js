cefba1f4f2c7ef2e905e120df4629edbaa8f8c76
joeyswafford <joey@swafford.co>
2021-09-18 14:14:07 -0400

    add zipcode functionality

---
+++ public/js/utils.js
@@ -0,0 +1,7 @@
+function changeZip() {
+    let newZip = document.getElementById("myInput").value;
+    let oldZipCode = document.getElementById("zipCode");
+
+    oldZipCode.textContent = newZip
+
+};
